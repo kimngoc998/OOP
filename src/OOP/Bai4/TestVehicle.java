5ce9e735fed65472d622f98617cb5e7329ebc364
ASUS <onkimngoc@gmail.com>
2021-03-15 21:13:50 +0700

    first commit

--- src/OOP/Bai4/TestVehicle.java
+++ src/OOP/Bai4/TestVehicle.java
@@ -2,7 +2,7 @@ package OOP.Bai4;
 
 public class TestVehicle {
     public static void main(String[] args) {
-        Vehicle v1 = new Vehicle("Nguyễn Thu Loan","Future Neo", 100,35000000);
+        Vehicle v1 = new Vehicle("Nguyễn Thu Loan","Future Neo",100,35000000);
         Vehicle v2 = new Vehicle("Lê Minh Tính","Ford Ranger", 3000,250000000);
         Vehicle v3 = new Vehicle("Nguyễn Minh Triết","LandScape", 1500,1000000000);
 
